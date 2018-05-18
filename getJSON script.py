8ce3462d6de9ab4e1dd97d7596ae72ae648074e0
elizachary99 <elizachary99@gmail.com>
2018-05-18 15:15:26 -0400

    Pagination (finally) implemented.

--- getJSON script.py
+++ getJSON script.py
@@ -1,4 +1,5 @@
 import requests
+import json
      
 with open("secrets.txt") as f:
     secrets = f.read()
@@ -14,6 +15,15 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
-with open("seeclickfix.json", "w+") as f:
-    f.truncate()
-    f.write(response.text)
+pages = response.json()["metadata"]["pagination"]["pages"]
+
+with open("seeclickfix_test.json", "w+") as f:
+    data = {}
+    data["issues"] = []
+    page = response.json()["metadata"]["pagination"]["page"]
+    while(page != None):
+        querystring = {"request_types":"11386","page":page,"details":"true"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        data["issues"] += response.json()["issues"]
+        page = response.json()["metadata"]["pagination"]["next_page"]
+    f.write(json.dumps(data))
