712378b40135120d559b77ba0d1bc8eba4e7485a
maryhalv <39344430+maryhalv@users.noreply.github.com>
2018-05-30 15:58:02 -0400

    Working sumTotal function

--- summary_table.js
+++ summary_table.js
@@ -2,7 +2,7 @@
         var request_type;
         var requests_total;
         $(document).ready(function () {
-            
+       
             //have a $.getJson for each request type??
             $.getJSON("https://townofchapelhill.github.io/scf-reconciliation/seeclickfix_test.json",
             function processData(jsonData) {
@@ -13,6 +13,16 @@
             
                 $(".table").append("<tr><td>"+ request_type + "</td><td>"+ requests_total + "</td><td>" +  sumTotal + "</td></tr>");
             });
+            
+             $.getJSON("https://townofchapelhill.github.io/scf-reconciliation/seeclickfix.json",
+            function processData(json_Data) {
+                request_type = getRequestType(json_Data);
+                requests_total = getRequestsTotal(json_Data);
+                getSumTotal(json_Data);
+            
+                $(".table").append("<tr><td>"+ request_type + "</td><td>"+ requests_total + "</td><td>" + sumTotal + "</td></tr>");
+            });
+            
        
        function getRequestType(jsonData) {
         while(jsonData.issues[0].request_type.title !== null) {
@@ -26,13 +36,18 @@
        }
        
        function getSumTotal(jsonData) {
+        sumTotal = 0;
         $.each(jsonData.issues, function (object, objectData) {
-                while(objectData.questions !== null && !isNaN(objectData.questions[1].answer)) {
+                if(objectData.questions !== null && !isNaN(objectData.questions[1].answer)) {
                  sumTotal += parseInt(objectData.questions[1].answer);
+                 console.log(sumTotal);
                 }
        });
-        if(sumTotal === undefined) {
-                    sumTotal = "Amount not updated";
+        
+        if(sumTotal === undefined || isNaN(sumTotal)) {
+                    sumTotal = "Amount not Updated";
+                } else {
+                    sumTotal = parseInt(sumTotal);
                 }
        }
        
