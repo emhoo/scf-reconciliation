ff7a27eb9d5fc6ab54f5fac62314fc880287975d
maryhalv <39344430+maryhalv@users.noreply.github.com>
2018-05-30 13:46:26 -0400

    handling only test file

---
+++ summary_table.js
@@ -0,0 +1,33 @@
+        var sumTotal;
+        var request_type;
+        var requests_total;
+        $(document).ready(function () {
+            
+            //have a $.getJson for each request type??
+            $.getJSON("https://townofchapelhill.github.io/scf-reconciliation/seeclickfix_test.json",
+            function processData(jsonData) {
+    
+                request_type = getRequestType(jsonData);
+                requests_total = getRequestsTotal(jsonData);
+                getSumTotal(jsonData);
+            
+                $(".table").append("<tr><td>"+ request_type + "</td><td>"+ requests_total + "</td><td>" +  sumTotal + "</td></tr>");
+            });
+       
+       function getRequestType(jsonData) {
+        return jsonData.issues[0].request_type.title;
+       }
+       
+       function getRequestsTotal(jsonData) {
+        return jsonData.issues.length;
+       }
+       
+       function getSumTotal(jsonData) {
+        $.each(jsonData.issues, function (object, objectData) {
+                while(objectData.questions !== null && !isNaN(objectData.questions[1].answer)) {
+                 sumTotal += parseInt(objectData.questions[1].answer);
+                }
+       });
+       }
+       
+        });
