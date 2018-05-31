9c3de2f9f732ce720b40be50802a658af1fe5ee7
elizachary99 <elizachary99@gmail.com>
2018-05-31 15:55:22 -0400

    Add files via upload

--- getJSON script.py
+++ getJSON script.py
@@ -5,9 +5,12 @@ import datetime
 with open("secrets.txt") as f:
     secrets = f.read()
 
+with open("config_getJSON.txt") as f:
+    config = f.read().split(",")
+
 url = "https://int.seeclickfix.com/api/v2/issues/"
 
-querystring = {"request_types":"11386","details":"true"}
+querystring = {"request_types":config[0],"details":"true"}
 
 headers = {
     'Authorization': secrets,
@@ -22,13 +25,14 @@ with open("metadata.txt", "w+") as f:
     f.truncate()
     f.write(datetime.datetime.now().strftime("%Y-%m-%d"))
 
-with open("seeclickfix.json", "w+") as f:
-    data = {}
-    data["issues"] = []
-    page = response.json()["metadata"]["pagination"]["page"]
-    while(page != None):
-        querystring = {"request_types":"11386","page":page,"details":"true"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        data["issues"] += response.json()["issues"]
-        page = response.json()["metadata"]["pagination"]["next_page"]
-    f.write(json.dumps(data))
+for c in config:
+    with open("data/seeclickfix_" + c + ".json", "w+") as f:
+        data = {}
+        data["issues"] = []
+        page = response.json()["metadata"]["pagination"]["page"]
+        while(page != None):
+            querystring = {"request_types":c,"page":page,"details":"true"}
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            data["issues"] += response.json()["issues"]
+            page = response.json()["metadata"]["pagination"]["next_page"]
+        f.write(json.dumps(data))
