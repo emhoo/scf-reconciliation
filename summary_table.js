86bfeeaf57cf9f6fbe5678fcdb91a8504a0ae7a3
maryhalv <39344430+maryhalv@users.noreply.github.com>
2018-05-31 15:26:46 -0400

    runs with config file

--- summary_table.js
+++ summary_table.js
@@ -1,29 +1,58 @@
         var sumTotal;
         var request_type;
         var requests_total;
+        var _year;
+		var _month;
+		var _day;
+		var display_date;
+		var previous_day;
         $(document).ready(function () {
-       
-            //have a $.getJson for each request type??
-            $.getJSON("https://townofchapelhill.github.io/scf-reconciliation/seeclickfix_test.json",
-            function processData(jsonData) {
+            
+            display_date = new Date();
+			console.log(display_date);
+				
+			_year = parseInt(display_date.getFullYear());
+			 _month = parseInt(display_date.getMonth()) + 1;
+			 _day = parseInt(display_date.getDate()) -1;
+
+			 if(display_date.getMonth().length == 2) {
+			 previous_day = _year+ "-" + _month + "-" + _day;
+			 } else {
+				previous_day = _year + "-0" + _month + "-" + _day;
+			 }
+             
+       var config;
+       $.get("https://townofchapelhill.github.io/scf-reconciliation/sum_table.txt", function(data) {
+				config = data.split("\n");
+				for(var s in config) {
+                    $.getJSON(config[s], function processData(jsonData) {
     
                 request_type = getRequestType(jsonData);
                 requests_total = getRequestsTotal(jsonData);
-                getSumTotal(jsonData);
+                if(jsonData.issues[0].request_type.id === 14437) {
+                    console.log("Test");
+                 if(requests_total === "No requests from today."){
+                sumTotal = "Amount not updated.";
+                } else {
+                    sumTotal = requests_total * 30;
+                    }
+                } else if(jsonData.issues[0].request_type.id === 14550) {
+                    if(requests_total === "No requests from today."){
+                sumTotal = "Amount not updated.";
+                } else {
+                    sumTotal = requests_total * 31;
+                    }
+                } else {
+                    getSumTotal(jsonData);
+                }
+            
             
                 $(".table").append("<tr><td>"+ request_type + "</td><td>"+ requests_total + "</td><td>" +  sumTotal + "</td></tr>");
+            });    
+                }
+        
             });
-            
-             $.getJSON("https://townofchapelhill.github.io/scf-reconciliation/seeclickfix.json",
-            function processData(json_Data) {
-                request_type = getRequestType(json_Data);
-                requests_total = getRequestsTotal(json_Data);
-                getSumTotal(json_Data);
-            
-                $(".table").append("<tr><td>"+ request_type + "</td><td>"+ requests_total + "</td><td>" + sumTotal + "</td></tr>");
-            });
-            
-       
+        
        function getRequestType(jsonData) {
         while(jsonData.issues[0].request_type.title !== null) {
         return jsonData.issues[0].request_type.title;
@@ -32,23 +61,125 @@
        }
        
        function getRequestsTotal(jsonData) {
-        return jsonData.issues.length;
+         var total = 0;
+       $.each(jsonData.issues, function (object, objectData) {
+         	if(parseInt(objectData.created_at.slice(0,4)) === _year
+					   && parseInt(objectData.created_at.slice(5,7)) === _month
+					   && parseInt(objectData.created_at.slice(8,10)) === _day + 1 && parseInt(objectData.created_at.slice(11,13)) < 17
+				   || objectData.created_at.slice(0,10) === previous_day && parseInt(objectData.created_at.slice(11,13)) >= 17 )  {
+                    total++;
+            }  
+        });
+       if(total === 0) total = "No requests from today.";
+       return total;
        }
        
        function getSumTotal(jsonData) {
         sumTotal = 0;
         $.each(jsonData.issues, function (object, objectData) {
+            	if(parseInt(objectData.created_at.slice(0,4)) === _year
+					   && parseInt(objectData.created_at.slice(5,7)) === _month
+					   && parseInt(objectData.created_at.slice(8,10)) === _day + 1 && parseInt(objectData.created_at.slice(11,13)) < 17
+				   || objectData.created_at.slice(0,10) === previous_day && parseInt(objectData.created_at.slice(11,13)) >= 17 ) {
                 if(objectData.questions !== null && !isNaN(objectData.questions[1].answer)) {
                  sumTotal += parseInt(objectData.questions[1].answer);
-                 console.log(sumTotal);
+
+                }
                 }
        });
-        
-        if(sumTotal === undefined || isNaN(sumTotal)) {
-                    sumTotal = "Amount not Updated";
+        if(sumTotal === undefined || isNaN(sumTotal) || sumTotal === 0) {
+                    sumTotal = "Amount not updated.";
                 } else {
                     sumTotal = parseInt(sumTotal);
                 }
        }
        
         });
+        
+        /*CODE FOR FILTER BUTTON*/
+        
+        function setDisplayDate() {
+            $(".table").empty();
+			$(".table").append( "<tr><th>Request Type</th><th>Total Request Count</th><th>Total Amount</th></tr>");
+			$(".norequests").empty();
+			
+			display_date = document.getElementById("filter_date").value;
+
+			 _year = parseInt(display_date.slice(0,4));
+			 _month = parseInt(display_date.slice(5,7));
+			 _day = parseInt(display_date.slice(8,11)) -1;
+			if(_month.length == 2) {
+			 previous_day = _year+ "-" + _month + "-" + _day;
+			 } else {
+				previous_day = _year + "-0" + _month + "-" + _day;
+			 }
+             
+              var config;
+       $.get("https://townofchapelhill.github.io/scf-reconciliation/sum_table.txt", function(data) {
+				config = data.split("\n");
+				for(var s in config) {
+                    $.getJSON(config[s], function processData(jsonData) {
+    
+                request_type = getRequestType(jsonData);
+                requests_total = getRequestsTotal(jsonData);
+               if(jsonData.issues[0].request_type.id === 14437) {
+                 if(requests_total === "No requests from today."){
+                sumTotal = "Amount not updated.";
+                } else {
+                    sumTotal = requests_total * 30;
+                console.log("reached");
+                    }
+                } else if(jsonData.issues[0].request_type.id === 14550) {
+                    if(requests_total === "No requests from today."){
+                sumTotal = "Amount not updated.";
+                } else {
+                    sumTotal = requests_total * 31;
+                    }
+                } else {
+                    getSumTotal(jsonData);
+                }
+            
+                $(".table").append("<tr><td>"+ request_type + "</td><td>"+ requests_total + "</td><td>" +  sumTotal + "</td></tr>");
+            });    
+                }
+        
+            });
+            
+       function getRequestType(jsonData) {
+        while(jsonData.issues[0].request_type.title !== null) {
+        return jsonData.issues[0].request_type.title;
+        }
+        return "Type not listed";
+       }
+       
+       function getRequestsTotal(jsonData) {
+        var total = 0;
+       $.each(jsonData.issues, function (object, objectData) {
+         if(objectData.created_at.slice(0,10) === display_date  && parseInt(objectData.created_at.slice(11,13)) < 17
+				   || objectData.created_at.slice(0,10) === previous_day && parseInt(objectData.created_at.slice(11,13)) >= 17 ) {
+                    total++;
+            }  
+        });
+       if(total === 0) total = "No requests in this time range.";
+       return total;
+       }
+       
+       function getSumTotal(jsonData) {
+        sumTotal = 0;
+        $.each(jsonData.issues, function (object, objectData) {
+            if(objectData.created_at.slice(0,10) === display_date  && parseInt(objectData.created_at.slice(11,13)) < 17
+				   || objectData.created_at.slice(0,10) === previous_day && parseInt(objectData.created_at.slice(11,13)) >= 17 ) {
+                if(objectData.questions !== null && !isNaN(objectData.questions[1].answer)) {
+                 sumTotal += parseInt(objectData.questions[1].answer);
+                }
+            }
+        });
+        
+        if(sumTotal === undefined || isNaN(sumTotal) || sumTotal === 0) {
+            sumTotal = "Amount not updated.";
+        } else {
+          sumTotal =  parseInt(sumTotal);
+        }
+       }
+       
+        }
